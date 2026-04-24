b0b9c54942d285f115f187f657b43c452ffa57be
eucbudak <e.ucbudak@gmail.com>
2026-04-24 09:17:59 +0300

    add custom domain kntakademi.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eucbudak.github.io',
+  site: 'https://kntakademi.com',
   vite: {
     plugins: [tailwindcss()]
   }
